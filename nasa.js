5dc4a110c9f22f89a6168cddb1db11a2231a5965
Mark Pearyer <Marcusp619@gmail.com>
2017-12-10 12:20:55 -0600

    started assigning values

--- nasa.js
+++ nasa.js
@@ -1,6 +1,7 @@
 // fetch data from NASA Image API using AJAX
 let url = "https://api.nasa.gov/planetary/apod?api_key=9YJNsiQnROfo8qztvtdYRk1xFdoksXy0j7L8ryiJ";
 
+let response = null;
 
 document.getElementsByTagName("button")[0].addEventListener("click", () => {
   searchImg(document.getElementsByTagName("input")[0].value);
@@ -10,6 +11,12 @@ var searchImg = (image) => {
   fetch("https://images-api.nasa.gov/search?q=" + image)
     .then( r => r.json())
     .then(d => {
+      response = d;
       console.log(d); // eslint-disable-line no-console
     }); 
+};
+
+var assignVal= () => {
+  let {collection, item, description, title} = response;
+  
 };
